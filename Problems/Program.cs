c972d9e7dfc82222fbf357404d09210c6810b636
Ryan McDonald <ryandennismcdonald@gmail.com>
2019-02-27 14:17:43 +0200

    Tests written

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public string Prefix(string input)
+        {
+            if (input == null) // if input is null
+            {
+                return null; 
+            }
+
+            string ans = "";
+            string len = (input.Length).ToString();
+            string[] ssize = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string wcount = (ssize.Length).ToString(); 
+            ans = len + "," + wcount + ":" + input; 
+            return ans;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //string input = Console.ReadLine();
+            Program p = new Program();
+            Console.WriteLine(p.Prefix("Hello World"));
         }
+
+        
     }
 }
